5fa8b972af9caefc3f5b4edadadc07ca2e9efa51
Jerry Dong <rtdong9421@yahoo.com>
2020-06-09 15:58:54 -0400

    basic file structure

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // 1. Displays food search on left and cocktail search on right
   // 1.1. Food pulls from themealdb.com api and drinks pulls from thecocktaildb.com api
-  // 1.2. User wil lbe able to search based on main ingredient/alcohol 
+  // 1.2. User will be able to search based on main ingredient/alcohol 
+    // 1.2.1. Dropdown menu
+    // *1.2.2. Free input
   // *1.3. User can filter results based on category or region
+
 // 2. Takes user input of preferred ingredient
   // 2.1. App searches from database and displays random result from list
+
 // 3. Modal popup of recommended recipe
   // 3.1. Choice to close modal/be redirected to cocktail selection
   // *3.2. Button to reroll random result recommendation
+
 // *4. Random meal and random cocktail function
+
 // *5. Nutritional fact option for each ingredent listed
+
 // *6. Design main page to toggle between food and drink search
 
 // *Stretch goals
